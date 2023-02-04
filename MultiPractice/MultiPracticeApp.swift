9ac44a41cea11e7589817310e4cb4ff8aba692ae
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-03 22:34:05 -0300

    Initial Commit

---
+++ MultiPractice/MultiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPracticeApp.swift
+//  MultiPractice
+//
+//  Created by Matheus Viana on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
