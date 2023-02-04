20bc177278b57072cebfcf45ad4d3c1987a130b9
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-03 22:54:11 -0300

    Add text asking how many questions they want to be asked

--- MultiPractice/ContentView.swift
+++ MultiPractice/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var table = 2
-    @State private var questions = 0
+    @State private var questionsToBeAsked = 5
+    
+    let questions = [5, 10, 20]
     
     var body: some View {
         VStack {
-            Text("Which table you'd like to practice today")
+            Text("Select the multiplication table you'd like to practice")
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
             
-            Stepper("Select table", value: $table, in: 2...12)
+            Stepper("Up to...", value: $table, in: 2...12)
+            Text("Selected table \(table)")
+            
+            Text("How many questions you'd like to practice")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+            
+            Picker("Select questions", selection: $questionsToBeAsked) {
+                ForEach(questions, id: \.self) {
+                    Text($0, format: .number)
+                }
+            }
+            .pickerStyle(.segmented)
         }
         .padding()
     }
