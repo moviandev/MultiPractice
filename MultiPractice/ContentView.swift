e5e0b96c251f9706e41d42d99951b5368185ef21
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-06 23:01:18 -0300

    Add toolbar button to change question and generate the questions again

--- MultiPractice/ContentView.swift
+++ MultiPractice/ContentView.swift
@@ -16,7 +16,7 @@ struct Question {
     }
     
     var question: String {
-        "How much is... \(multiplicant) x \(multiplicator)"
+        "What is... \(multiplicant) x \(multiplicator)"
     }
 }
 
@@ -25,6 +25,8 @@ struct ContentView: View {
     @State private var table = 2
     @State private var questionNumber = Int.random(in: 0...2)
     @State private var score = 0
+    @State private var answer = 0
+    @State private var playedRounds = 0
     
     @State private var questions: [Question] = []
     @State private var question = ""
@@ -35,54 +37,59 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 Spacer()
+                Spacer()
                 Text("MultiPractice")
                     .font(.largeTitle.bold())
                     .foregroundColor(.indigo)
-                
-                VStack(spacing: 15) {
-                    Spacer()
-                    
+
+                VStack(spacing: 50) {
                     Text("Choose how many questions you want")
                         .foregroundColor(.secondary)
                         .font(.subheadline)
-                    
+
                     Picker("Questions", selection: $rounds) {
                         ForEach(roundsQuantity, id: \.self) {
                             Text($0, format: .number)
                         }
                     }
                     .pickerStyle(.segmented)
-                    
-                    Spacer()
-                    
-                    Stepper("Choose the multiplication table", value: $table, in: 2...12)
+
+                    Stepper("Current table \(table)", value: $table, in: 2...12)
                         .foregroundColor(.secondary)
                         .font(.subheadline)
-                    
-                    
+
+
                     Text(question)
-                    
-                    HStack(spacing: 50) {
-                        ForEach(0..<3) { question in
-                            Button("\(questions[question].product)") {
-                                validateAnswer(answer: question)
-                            }
+
+                    TextField("Your answer", value: $answer, format: .number)
+                        .onSubmit {
+                            validateAnswer(answer: answer)
                         }
-                    }
-                    
+
                     Spacer()
-                    
+
                     Text("Your current score \(score)")
-                    
+
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button("Practice Again") {
+                        print(question)
+                        startGame()
+                        print(question)
+                        print(table)
+                    }
                 }
             }
         }
+
         .padding()
         .onAppear(perform: startGame)
     }
     
     func validateAnswer(answer: Int) {
-        if questions[questionNumber].product == questions[answer].product {
+        if questions[questionNumber].product == answer {
             score += 1
         } else {
             score -= score > 1 ? 1 : 0
@@ -90,6 +97,10 @@ struct ContentView: View {
     }
     
     func startGame() {
+        if questions.count > 0 {
+            questions.removeAll()
+        }
+        
         for _ in 0..<rounds {
             questions.append(Question(multiplicant: Int.random(in: 2...12), multiplicator: table))
         }
